091b8e70c21a3659c9d1bb43433a28b5b7a33677
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-02-03 16:31:02 -0300

    feat: typing app instance

--- server/src/infra/api/adapters/FastifyAdapter/FastifyAdapter.ts
+++ server/src/infra/api/adapters/FastifyAdapter/FastifyAdapter.ts
@@ -1,15 +1,17 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import Fastify from 'fastify';
+import Fastify, { FastifyInstance } from 'fastify';
+import cors from '@fastify/cors'
 import HttpServer from 'infra/api/http/HttpServer';
 
 export default class FastifyAdapter implements HttpServer {
-  app: any;
+  app: FastifyInstance;
 
   constructor() {
     this.app = Fastify();
+    this.app.register(cors)
   }
 
-  on(method: string, url: string, callback: Function): void {
+  on(method: 'get' | 'post' | 'delete' | 'put', url: string, callback: Function): void {
     this.app[method](url, async function () {
       const output = await callback();
       return output;
