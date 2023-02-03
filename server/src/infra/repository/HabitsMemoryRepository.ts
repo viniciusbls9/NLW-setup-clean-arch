27425f0aee6739f7fe48d7b1a718e5bf06faa5aa
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-02-03 07:55:34 -0300

    refactor: create mockHabits

--- server/src/infra/repository/HabitsMemoryRepository.ts
+++ server/src/infra/repository/HabitsMemoryRepository.ts
@@ -1,21 +1,22 @@
 import { HabitsRepository } from "domain/repositories/HabitsRepository";
 
+export const mockHabits = [
+  {
+    id: "f564aa97-d649-410e-b2f0-70cfe9dae2e2",
+    title: "Beber água",
+    create_at: "1970-01-01T00:00:00.000Z"
+  },
+  {
+    id: "d61207cd-4ff2-4908-a016-d3cdc3a2b9cd",
+    title: "Teste",
+    create_at: "1970-01-01T00:00:00.000Z"
+  }
+];
 export default class HabitsMemoryRepository implements HabitsRepository {
   habits: any[];
 
   constructor() {
-    this.habits = [
-      {
-        id: "f564aa97-d649-410e-b2f0-70cfe9dae2e2",
-        title: "Beber água",
-        create_at: "1970-01-01T00:00:00.000Z"
-      },
-      {
-        id: "d61207cd-4ff2-4908-a016-d3cdc3a2b9cd",
-        title: "Teste",
-        create_at: "1970-01-01T00:00:00.000Z"
-      }
-    ];
+    this.habits = mockHabits;
   }
 
   async findAll(): Promise<any[]> {
