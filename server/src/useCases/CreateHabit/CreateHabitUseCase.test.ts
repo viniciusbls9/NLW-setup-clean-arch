905fa74a50e87159f010783973e41718774b03c6
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-02-09 17:03:41 -0300

    test: ensure call AddHabits.execute and create new habits

--- server/src/useCases/CreateHabit/CreateHabitUseCase.test.ts
+++ server/src/useCases/CreateHabit/CreateHabitUseCase.test.ts
@@ -1,13 +1,13 @@
 import HabitsMemoryRepository from "infra/repository/HabitsMemoryRepository";
 import { describe, test, expect } from "vitest";
-import { CreateHabit } from "./CreateHabitUseCase";
+import { CreateHabitUseCase } from "./CreateHabitUseCase";
 
 
 describe('AddHabits', () => {
     test('should call AddHabits.execute and create new habits', async () => {
         const createMemoryHabit = new HabitsMemoryRepository()
-        const createHabit = new CreateHabit(createMemoryHabit)
-        const response = await createHabit.execute({title: 'Beber 2L de água', weekDays: [1,3,5]})
+        const createHabit = new CreateHabitUseCase(createMemoryHabit)
+        const response = await createHabit.execute({ title: 'Beber 2L de água', weekDays: [1,3,5] })
 
         expect(response).toBe('Great! Habit created with success')
         
