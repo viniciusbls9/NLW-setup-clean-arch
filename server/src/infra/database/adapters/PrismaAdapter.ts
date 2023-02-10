bc2b1e026a94756962545a4bbd78bc3cdf9006fc
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-02-10 07:04:47 -0300

    feat: create createHabit method

--- server/src/infra/database/adapters/PrismaAdapter.ts
+++ server/src/infra/database/adapters/PrismaAdapter.ts
@@ -1,15 +1,31 @@
 import Connection from "./Connection";
 import { PrismaClient } from '@prisma/client'
-
+import { CreateHabitDTO } from "useCases/CreateHabit/CreateHabitDTO";
 export default class PrismaAdpater implements Connection {
-    connection: any
+  connection = new PrismaClient()
 
-    constructor() {
-        this.connection = new PrismaClient()
-    }
+  constructor() { }
 
-    getAllHabits(): Promise<any> {
-        return this.connection.habit.findMany()
-    }
-    
+  async createHabit({ title, weekDays }: CreateHabitDTO): Promise<string> {
+    await this.connection.habit.create({
+      data: {
+        title,
+        created_at: new Date(),
+        weekDays: {
+          create: weekDays.map(weekDay => {
+            return {
+              week_day: weekDay
+            }
+          })
+        }
+      }
+    })
+
+    return 'Great! Habit created with success'
+  }
+
+  getAllHabits(): Promise<any> {
+    return this.connection.habit.findMany()
+  }
+
 }
