d5d55a292da2b34e62e7edd0f3817394c46240a6
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-02-10 07:36:06 -0300

    refactor: refactor test

--- server/src/infra/api/router/router.test.ts
+++ server/src/infra/api/router/router.test.ts
@@ -12,12 +12,12 @@ describe("Router", () => {
       {
         id: "f564aa97-d649-410e-b2f0-70cfe9dae2e2",
         title: "Beber água",
-        create_at: "1970-01-01T00:00:00.000Z"
+        created_at: "1970-01-01T00:00:00.000Z"
       },
       {
         id: "d61207cd-4ff2-4908-a016-d3cdc3a2b9cd",
         title: "Teste",
-        create_at: "1970-01-01T00:00:00.000Z"
+        created_at: "1970-01-01T00:00:00.000Z"
       }
     ])
   });
