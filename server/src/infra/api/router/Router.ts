26861d62f418d69586669732274238598f9bc3aa
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-02-10 07:02:21 -0300

    feat: create habits router

--- server/src/infra/api/router/Router.ts
+++ server/src/infra/api/router/Router.ts
@@ -1,6 +1,12 @@
 import HttpServer from 'infra/api/http/HttpServer';
 import { HabitsRepository } from 'domain/repositories/HabitsRepository';
 import { ListHabitsUseCase } from 'useCases/ListHabits/ListHabitsUseCase';
+import { CreateHabitUseCase } from 'useCases/CreateHabit/CreateHabitUseCase';
+
+interface RequestProps {
+  body: { [key: string]: any }
+  params: { [key: string]: any }
+}
 
 export default class Router {
   constructor(readonly httpServer: HttpServer, readonly habitsRepository: HabitsRepository) { }
@@ -11,6 +17,12 @@ export default class Router {
       const habits = await listHabitsUseCase.execute()
 
       return habits
+    });
+
+    this.httpServer.on('post', '/habits', async (request: RequestProps) => {
+      const createHabit = new CreateHabitUseCase(this.habitsRepository)
+      const { title, weekDays } = request.body
+      await createHabit.execute({ title, weekDays })
     });
   }
 }
