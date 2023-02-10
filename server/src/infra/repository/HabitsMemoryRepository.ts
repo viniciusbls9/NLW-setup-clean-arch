477f2af9be5d2380e06e3acbca3168aec4b83178
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-02-10 07:36:34 -0300

    refactor: refactor HabitsMemoryRepository class

--- server/src/infra/repository/HabitsMemoryRepository.ts
+++ server/src/infra/repository/HabitsMemoryRepository.ts
@@ -5,12 +5,12 @@ export const mockHabits = [
   {
     id: "f564aa97-d649-410e-b2f0-70cfe9dae2e2",
     title: "Beber água",
-    create_at: "1970-01-01T00:00:00.000Z"
+    created_at: "1970-01-01T00:00:00.000Z"
   },
   {
     id: "d61207cd-4ff2-4908-a016-d3cdc3a2b9cd",
     title: "Teste",
-    create_at: "1970-01-01T00:00:00.000Z"
+    created_at: "1970-01-01T00:00:00.000Z"
   }
 ];
 export default class HabitsMemoryRepository implements HabitsRepository {
@@ -20,11 +20,15 @@ export default class HabitsMemoryRepository implements HabitsRepository {
     this.habits = mockHabits;
   }
 
+  async findDayDetails(): Promise<any[]> {
+    return this.habits
+  }
+
   async create({ title, weekDays }: CreateHabitDTO): Promise<string> {
     this.habits.push({
       title,
       weekDays,
-      create_at: "1970-01-01T00:00:00.000Z"
+      created_at: "1970-01-01T00:00:00.000Z"
     })
 
     return 'Great! Habit created with success'
