b2f551f43eb28ed67e78ba7b788515f66085f935
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-02-10 07:18:40 -0300

    feat: create findDayDetails on HabitsRepository interface

--- server/src/domain/repositories/HabitsRepository.ts
+++ server/src/domain/repositories/HabitsRepository.ts
@@ -3,4 +3,5 @@ import { CreateHabitDTO } from "useCases/CreateHabit/CreateHabitDTO"
 export interface HabitsRepository {
     findAll(): Promise<any[]> // change to Habits[] interface
     create({title, weekDays}: CreateHabitDTO): Promise<string>
+    findDayDetails(): Promise<any>
 }
