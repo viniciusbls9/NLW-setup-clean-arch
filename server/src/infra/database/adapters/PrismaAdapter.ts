a8dbf00f49de548e7576f160c8c32fa9a59a97ec
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-02-10 11:43:38 -0300

    feat: create getDayDetails method on PrismaAdpater class

--- server/src/infra/database/adapters/PrismaAdapter.ts
+++ server/src/infra/database/adapters/PrismaAdapter.ts
@@ -6,6 +6,27 @@ export default class PrismaAdpater implements Connection {
 
   constructor() { }
 
+  async getDayDetails(date: string): Promise<any> {
+    const weekDay = Number(date.split('-')[2].split('T')[0])
+
+    const possibleHabits = await this.connection.habit.findMany({
+      where: {
+        created_at: {
+          lte: date
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay
+          }
+        }
+      }
+    })
+    
+    return {
+      possibleHabits
+    }
+  }
+
   async createHabit({ title, weekDays }: CreateHabitDTO): Promise<string> {
     await this.connection.habit.create({
       data: {
@@ -24,7 +45,7 @@ export default class PrismaAdpater implements Connection {
     return 'Great! Habit created with success'
   }
 
-  getAllHabits(): Promise<any> {
+  getAllHabits(): Promise<any[]> {
     return this.connection.habit.findMany()
   }
 
