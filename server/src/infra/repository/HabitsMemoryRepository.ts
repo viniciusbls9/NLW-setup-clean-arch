120261036400b0550dbe11ada1e705552535a00a
Vinicius Benedito <beneditoloura@gmail.com>
2023-02-10 16:36:31 -0300

    refactor: change default habits mock

--- server/src/infra/repository/HabitsMemoryRepository.ts
+++ server/src/infra/repository/HabitsMemoryRepository.ts
@@ -4,13 +4,18 @@ import { CreateHabitDTO } from "useCases/CreateHabit/CreateHabitDTO";
 export const mockHabits = [
   {
     id: "f564aa97-d649-410e-b2f0-70cfe9dae2e2",
-    title: "Beber água",
-    created_at: "1970-01-01T00:00:00.000Z"
+    title: "Drink Water",
+    created_at: "2023-02-01T00:00:00.000Z"
   },
   {
     id: "d61207cd-4ff2-4908-a016-d3cdc3a2b9cd",
-    title: "Teste",
-    created_at: "1970-01-01T00:00:00.000Z"
+    title: "Study Clean Arch",
+    created_at: "2023-02-05T00:00:00.000Z"
+  },
+  {
+    id: "d61207cd-4ff2-4908-a016-d3cdc3a2b9cd",
+    title: "Study backend",
+    created_at: "2023-02-05T00:00:00.000Z"
   }
 ];
 export default class HabitsMemoryRepository implements HabitsRepository {
@@ -20,8 +25,8 @@ export default class HabitsMemoryRepository implements HabitsRepository {
     this.habits = mockHabits;
   }
 
-  async findDayDetails(): Promise<any[]> {
-    return this.habits
+  async findDayDetails(date: string): Promise<any[]> {
+    return this.habits.filter(habit => habit.created_at === date)
   }
 
   async create({ title, weekDays }: CreateHabitDTO): Promise<string> {
