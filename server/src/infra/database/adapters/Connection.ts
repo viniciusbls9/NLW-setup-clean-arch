df6a1630503c66dcbdaf69ee4edfb944a151327b
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-02-10 08:04:13 -0300

    refactor: include date params on findDayDetails method

--- server/src/infra/database/adapters/Connection.ts
+++ server/src/infra/database/adapters/Connection.ts
@@ -3,5 +3,5 @@ import { CreateHabitDTO } from "useCases/CreateHabit/CreateHabitDTO"
 export default interface Connection {
     getAllHabits(): Promise<any[]>
     createHabit({ title, weekDays }: CreateHabitDTO): Promise<any>
-    getDayDetails(): Promise<any>
+    getDayDetails(date: string): Promise<any>
 }
--- server/src/infra/repository/HabitsDatabaseRepository.ts
+++ server/src/infra/repository/HabitsDatabaseRepository.ts
@@ -5,6 +5,10 @@ import { CreateHabitDTO } from "useCases/CreateHabit/CreateHabitDTO";
 export default class HabitsDatabaseRepository implements HabitsRepository {
     constructor(readonly connection: Connection) {}
 
+    findDayDetails(date: string): Promise<any[]> {
+        return this.connection.getDayDetails(date)    
+    }
+
     create({ title, weekDays }: CreateHabitDTO): Promise<string> {
         return this.connection.createHabit({ title, weekDays })
     }
