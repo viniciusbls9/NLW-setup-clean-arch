de2f25c610c3bbc8b4892e6bb4b7cdb2faf5684e
Vinicius Benedito <beneditoloura@gmail.com>
2023-02-12 12:38:57 -0300

    test: ensure call AddHabits.execute with wrong params and should not create new habits

--- server/src/useCases/CreateHabit/CreateHabitUseCase.test.ts
+++ server/src/useCases/CreateHabit/CreateHabitUseCase.test.ts
@@ -1,19 +1,31 @@
 import HabitsMemoryRepository from "infra/repository/HabitsMemoryRepository";
 import { describe, test, expect } from "vitest";
+import { ZodError } from "zod";
 import { CreateHabitUseCase } from "./CreateHabitUseCase";
 
 
 describe('AddHabits', () => {
-    test('should call AddHabits.execute and create new habits', async () => {
-        const createMemoryHabit = new HabitsMemoryRepository()
-        const createHabit = new CreateHabitUseCase(createMemoryHabit)
-        const habitData = {
-            title: 'Beber 2L de água', weekDays: [1,3,5] 
-        }
+  test('should call AddHabits.execute and create new habits', async () => {
+    const createMemoryHabit = new HabitsMemoryRepository()
+    const createHabit = new CreateHabitUseCase(createMemoryHabit)
+    const habitData = {
+      title: 'Beber 2L de água', weekDays: [1, 3, 5]
+    }
 
-        const response = await createHabit.execute(habitData)
+    const response = await createHabit.execute(habitData)
 
-        expect(response).toBe('Great! Habit created with success')
-        
-    })
+    expect(response).toBe('Great! Habit created with success')
+  })
+
+  test('should call AddHabits.execute with wrong params and should not create new habits', async () => {
+    const createMemoryHabit = new HabitsMemoryRepository()
+    const createHabit = new CreateHabitUseCase(createMemoryHabit)
+    const habitData = {
+      title: 6, weekDays: [1, 3, 5]
+    }
+
+    const response = await createHabit.execute(habitData)
+
+    expect(response).toBeInstanceOf(ZodError)
+  })
 })
