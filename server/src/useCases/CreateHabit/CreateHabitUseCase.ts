cf3ceee712effaa16dee5c19e54d68463213aa66
Vinicius Benedito <beneditoloura@gmail.com>
2023-02-12 12:39:39 -0300

    feat: create CreateHabitValidate class

--- server/src/useCases/CreateHabit/CreateHabitUseCase.ts
+++ server/src/useCases/CreateHabit/CreateHabitUseCase.ts
@@ -1,13 +1,17 @@
 import { Habit } from "domain/repositories/Habit";
 import { HabitsRepository } from "domain/repositories/HabitsRepository";
-import CreateHabitValidate from "useCases/validators/CreateHabitValidate";
+import CreateHabitValidate from "useCases/validators/CreateHabitValidate/CreateHabitValidate";
 
 export class CreateHabitUseCase {
-    constructor(private habitsRepository: HabitsRepository) {}
+  constructor(private habitsRepository: HabitsRepository) { }
 
-    async execute(habitData: Habit) {
-        const validate = new CreateHabitValidate(habitData)
-        validate.validate()
-        return this.habitsRepository.create(habitData)
+  async execute(habitData: Habit) {
+    try {
+      const validate = new CreateHabitValidate(habitData)
+      validate.validate()
+      return this.habitsRepository.create(habitData)
+    } catch (error) {
+      return error
     }
+  }
 }
