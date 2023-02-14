f2928fbe6beb9c939429fcf8858937bcfb586100
Vinicius Benedito <beneditoloura@gmail.com>
2023-02-14 08:53:03 -0300

    refactor: change function to const

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Header } from '@components/Header';
 import { SummaryTable } from '@components/SummaryTable';
 import './presentation/styles/global.css';
 
-export function App() {
+export const App = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
@@ -11,4 +11,4 @@ export function App() {
       </div>
     </div>
   );
-}
+};
--- frontend/src/presentation/components/Header/index.tsx
+++ frontend/src/presentation/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import logoImage from '@assets/logo.svg';
 import { Plus } from 'phosphor-react';
 
-export function Header() {
+export const Header = () => {
   return (
     <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
       <img src={logoImage} alt="Logo Habit" />
@@ -15,4 +15,4 @@ export function Header() {
       </button>
     </div>
   );
-}
+};
