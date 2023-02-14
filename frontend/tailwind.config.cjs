3ed2de7a1d28e29df2405152b2d6df9208f85e88
Vinicius Benedito <beneditoloura@gmail.com>
2023-02-14 08:09:52 -0300

    feat: create background default color and add logo

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: '#09090A'
+      }
+    }
   },
   plugins: []
 };
