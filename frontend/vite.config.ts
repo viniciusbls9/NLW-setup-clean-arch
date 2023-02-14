9944635539fa709d2d0eca8637916343fc2457ae
Vinicius Benedito <beneditoloura@gmail.com>
2023-02-14 08:38:38 -0300

    feat: use alias to improve imports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/presentation/assets'),
+      '@components': path.resolve(__dirname, './src/presentation/components')
+    }
+  }
+});
