0bde8387b6d95111d6115e601c045542fb1c5a8b
Vinicius Benedito <beneditoloura@gmail.com>
2023-02-15 07:24:50 -0300

    refactor: remove unused imports

--- frontend/src/presentation/components/Header/Header.test.tsx
+++ frontend/src/presentation/components/Header/Header.test.tsx
@@ -1,6 +1,5 @@
 import { describe, test, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Header } from '.';
 
