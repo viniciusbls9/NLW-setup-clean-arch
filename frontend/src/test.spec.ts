34567115a76264236a156b88783ff2c81a7f16a8
Vinicius Benedito <beneditoloura@gmail.com>
2023-02-15 07:22:44 -0300

    test: setup vitest and testing library

--- frontend/src/test.spec.ts
+++
@@ -1,5 +0,0 @@
-import { expect, test } from 'vitest';
-
-test('sum two number', () => {
-  expect(2 + 3).toBe(5);
-});
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import * as path from 'path';
@@ -5,6 +8,13 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['test/setup.ts'],
+    include: ['**/*(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache']
+  },
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, './src/presentation/assets'),
