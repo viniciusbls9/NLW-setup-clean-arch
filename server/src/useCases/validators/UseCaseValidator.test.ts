238abfd1a65a196e9e8e8162c0d3aeb8771e6e37
Vinicius Benedito <beneditoloura@gmail.com>
2023-02-27 07:00:08 -0300

    test: ensure return date if params are correct

--- server/src/useCases/validators/UseCaseValidator.test.ts
+++ server/src/useCases/validators/UseCaseValidator.test.ts
@@ -2,14 +2,22 @@ import { describe, test, expect } from 'vitest'
 import UseCaseValidator from './UseCaseValidator'
 
 describe('UseCaseValidator', () => {
-    test('Should return title and weekDays if params are correct', () => {
-        const params = {
-            title: 'Create Habit',
-            weekDays: [1,2,3]
-        }
+  test('Should return title and weekDays if params are correct', () => {
+    const params = {
+      title: 'Create Habit',
+      weekDays: [1, 2, 3]
+    }
 
-        const validate = new UseCaseValidator(params)
+    const validate = new UseCaseValidator(params)
 
-        expect(validate.createHabitValidator()).toEqual(params)
-    })
+    expect(validate.createHabitValidator()).toEqual(params)
+  })
+
+  test('Should return date if params are correct', async () => {
+    const params = { date: '2023-02-05T00:00:00.000Z' }
+
+    const validate = new UseCaseValidator(params)
+    
+    expect(validate.getDayDetailsValidator()).toBeTypeOf('object')
+  })
 })
