8ac4da085ce593926e0f8821db1c43c269ab7a4d
Vinicius Benedito <beneditoloura@gmail.com>
2023-03-07 07:19:11 -0300

    feat: using radix popover on habitDay component

--- frontend/src/presentation/components/HabitDay/index.tsx
+++ frontend/src/presentation/components/HabitDay/index.tsx
@@ -1,5 +1,20 @@
+import { HabitProgressBar } from '@components/HabitProgressBar';
+import * as Popover from '@radix-ui/react-popover'
+
 export const HabitDay = () => {
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <Popover.Root>
+      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          <span className='font-semibold text-zinc-400'>segunda-feira</span>
+          <span className='mt-1 font-extrabold leading-tight text-3xl'>17/01</span>
+
+          <HabitProgressBar progress={75} />
+          <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 };
