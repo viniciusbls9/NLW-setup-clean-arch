38763ca8f5b1e27133bac11c7e0a5e4fad0e0b8d
Vinicius Benedito <beneditoloura@gmail.com>
2023-03-07 07:51:47 -0300

    test: ensure show different color percentage based

--- frontend/src/presentation/components/HabitDay/HabitDay.test.tsx
+++ frontend/src/presentation/components/HabitDay/HabitDay.test.tsx
@@ -1,5 +1,5 @@
 import { describe, test, expect } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { HabitDay } from '.';
 
@@ -12,5 +12,12 @@ describe('HabitDay Component', () => {
   test('Should render correctly HabitDay component', () => {
     const { container } = render(<HabitDay {...props} />);
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('Should show different color percentage based', () => {
+    render(<HabitDay {...props} />);
+    expect(screen.getByRole('button')).toHaveStyle({
+      backgroundColor: 'rgb(139 92 246 / 1)'
+    });
   });
 });
