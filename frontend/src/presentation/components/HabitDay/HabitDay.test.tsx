dcd29172a24022875abfd320091e53afd1f8de99
Vinicius Benedito <beneditoloura@gmail.com>
2023-03-07 07:43:52 -0300

    refactor: create HabitDay props

--- frontend/src/presentation/components/HabitDay/HabitDay.test.tsx
+++ frontend/src/presentation/components/HabitDay/HabitDay.test.tsx
@@ -3,9 +3,14 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { HabitDay } from '.';
 
+const props = {
+  completed: 4,
+  amount: 5
+};
+
 describe('HabitDay Component', () => {
   test('Should render correctly HabitDay component', () => {
-    const { container } = render(<HabitDay />);
+    const { container } = render(<HabitDay {...props} />);
     expect(container.firstChild).toMatchSnapshot();
   });
 });
