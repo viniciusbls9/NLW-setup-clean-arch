a3081031c4376f75e425ac8b203b11eeb5cdd413
Vinicius Benedito <beneditoloura@gmail.com>
2023-03-08 06:53:25 -0300

    feat: using group-data to styles checkbox

--- frontend/src/presentation/components/HabitDay/index.tsx
+++ frontend/src/presentation/components/HabitDay/index.tsx
@@ -42,8 +42,8 @@ export const HabitDay = ({ amount, completed }: HabitDayProps) => {
           <HabitProgressBar progress={completedPercentage} />
 
           <div className="mt-6 flex flex-col gap-3">
-            <Checkbox.Root className="flex items-center gap-3">
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800">
+            <Checkbox.Root className="flex items-center gap-3 group">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500">
                 <Checkbox.Indicator>
                   <Check size={20} className="text-white" />
                 </Checkbox.Indicator>
