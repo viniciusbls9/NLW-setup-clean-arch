0677ce8998125aaba0e990dcd3419e37c8389a74
Vinicius Benedito <beneditoloura@gmail.com>
2023-03-08 06:48:18 -0300

    feat: create habit checkbox

--- frontend/src/presentation/components/HabitDay/index.tsx
+++ frontend/src/presentation/components/HabitDay/index.tsx
@@ -1,6 +1,8 @@
 import { HabitProgressBar } from '@components/HabitProgressBar';
 import * as Popover from '@radix-ui/react-popover';
+import * as Checkbox from '@radix-ui/react-checkbox';
 import clsx from 'clsx';
+import { Check } from 'phosphor-react';
 
 interface HabitDayProps {
   completed: number;
@@ -38,6 +40,20 @@ export const HabitDay = ({ amount, completed }: HabitDayProps) => {
           </span>
 
           <HabitProgressBar progress={completedPercentage} />
+
+          <div className="mt-6 flex flex-col gap-3">
+            <Checkbox.Root className="flex items-center gap-3">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+              <span className="font-semibold text-xl text-white leading-tight">
+                Beber 2L de água
+              </span>
+            </Checkbox.Root>
+          </div>
+
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
       </Popover.Portal>
