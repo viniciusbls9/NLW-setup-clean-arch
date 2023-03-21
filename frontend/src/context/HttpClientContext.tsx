c64e5ddfede00c327d1cf3972cb880d57b720f26
Vinicius Benedito <beneditoloura@gmail.com>
2023-03-21 08:48:37 -0300

    feat: create HttpClientContext

---
+++ frontend/src/context/HttpClientContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, PropsWithChildren, useContext } from 'react';
+import { HabitsRepository } from '@gateways/HabitGateway';
+import HabitHttpGateway from '@gateways/HabitHttpGateway';
+import AxiosAdapter from '@infra/api/adapter/AxiosAdapter';
+
+const httpClientInstance = new AxiosAdapter();
+
+export type HttpClientType = {
+  habitGateway: HabitsRepository;
+};
+
+const defaultContext: HttpClientType = {
+  habitGateway: new HabitHttpGateway(
+    httpClientInstance,
+    'http://localhost:3001'
+  )
+};
+
+export const HttpClientContext = createContext(defaultContext);
+
+export const HttpClientProvider = ({ children }: PropsWithChildren) => {
+  const habitGateway = new HabitHttpGateway(
+    httpClientInstance,
+    'http://localhost:3001'
+  );
+
+  return (
+    <HttpClientContext.Provider value={{ habitGateway }}>
+      {children}
+    </HttpClientContext.Provider>
+  );
+};
+
+export const useHttpClient = () => {
+  return useContext(HttpClientContext);
+};
