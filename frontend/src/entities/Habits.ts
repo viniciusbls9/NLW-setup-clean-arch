94e5098e85df2fec9bcd0dba3094ed8320ad29cf
Vinicius Benedito <beneditoloura@gmail.com>
2023-03-23 06:51:43 -0300

    feat: create habits entities

---
+++ frontend/src/entities/Habits.ts
@@ -0,0 +1,37 @@
+import { Habit } from '@gateways/HabitGateway';
+import Observable from './Observable';
+
+export default class Habits extends Observable {
+  items: any;
+
+  constructor(items?: any) {
+    super();
+    this.items = [];
+    if (items) {
+      for (const item of items) {
+        this.items.push({
+          id: item.id,
+          title: item.title
+        });
+      }
+    }
+  }
+
+  async create(habitData: Habit) {
+    this.items.push(habitData);
+    this.notify('addItem', habitData);
+  }
+
+  // async toggleHabit(item: any) {
+  //   item.done = !item.done;
+  //   this.notify('toggleDone', item);
+  // }
+
+  // summary(description: string) {
+  //   return this.items.find((item: any) => item.description === description);
+  // }
+
+  // findDayDetails() {}
+
+  // findAll() {}
+}
