c90f0d1d396fd0ff573c2de520906cd3e92e75ae
Vinicius Benedito <beneditoloura@gmail.com>
2023-03-23 07:10:11 -0300

    feat: using HttpClientProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react';
+import { HttpClientProvider } from '@context/HttpClientContext';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <HttpClientProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </HttpClientProvider>
 );
