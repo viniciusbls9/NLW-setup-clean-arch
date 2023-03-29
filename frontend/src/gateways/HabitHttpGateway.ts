ef95155ad7348d4721889a5284e0252f2fe4777d
Vinicius Benedito <beneditoloura@gmail.com>
2023-03-29 07:50:28 -0300

    feat: create new gateway

---
+++ frontend/src/gateways/HabitHttpGateway.ts
@@ -0,0 +1,29 @@
+import Habits from '@entities/Habits';
+import HttpServer from '@infra/api/http/HttpServer';
+import { DayDetails, Habit, HabitsRepository } from '@gateways/HabitGateway';
+
+export default class HabitHttpGateway implements HabitsRepository {
+  constructor(readonly httpClient: HttpServer, readonly baseUrl: string) {}
+
+  async findAll(): Promise<Habit[]> {
+    const habitData = await this.httpClient.get(`${this.baseUrl}/`);
+    const habits = new Habits(habitData);
+    return habits.findAll();
+  }
+
+  async create(habitData: Habit): Promise<any> {
+    await this.httpClient.post('/habits', habitData);
+  }
+
+  findDayDetails(date: string): Promise<DayDetails> {
+    throw new Error('Method not implemented.');
+  }
+
+  toggleHabit(id: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  summary(): Promise<unknown> {
+    throw new Error('Method not implemented.');
+  }
+}
