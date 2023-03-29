a852e53101050afbad02886be045f3f3a29e343d
Vinicius Benedito <beneditoloura@gmail.com>
2023-03-29 07:49:45 -0300

    refactor: change component three

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <HttpClientProvider>
-    <StrictMode>
+  <StrictMode>
+    <HttpClientProvider>
       <App />
-    </StrictMode>
-  </HttpClientProvider>
+    </HttpClientProvider>
+  </StrictMode>
 );
